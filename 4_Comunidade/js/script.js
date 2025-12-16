d2c1025ce047a541ddbb9761bc4472e06ae88b12
equipe-calmamente <calmamente.equipe@gmail.com>
2025-12-16 10:27:03 +0000

    forms e att header

--- 4_Comunidade/js/script.js
+++ 4_Comunidade/js/script.js
@@ -109,3 +109,12 @@ function toggleMenu() {
 
 btnMobile.addEventListener('click', toggleMenu);
 
+window.onscroll = function() { updateProgressBar() };
+
+function updateProgressBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
