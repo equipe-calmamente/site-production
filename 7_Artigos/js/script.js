525caa6e6b55320c14fbf05eae31e44d115e7414
ajtoriani <anajuliatoriani@gmail.com>
2025-12-16 09:11:49 -0300

    add artigos

---
+++ 7_Artigos/js/script.js
@@ -0,0 +1,102 @@
+const footerResponsivo = document.getElementById('footerResponsivo');
+const socialMediaAntigo = document.getElementById('redesAntiga')
+
+// Esta função roda quando o tamanho da tela muda
+window.addEventListener('resize', () => {
+  // Essa variavel pega o tamanho da tela atualizado
+  let tamanhoTela = window.innerWidth;
+  
+  console.log(footerResponsivo.classList.toString());
+  /* Esta condicional verifica se o tamanho da tel da variavel tamanhoTela é menor que o desejado para então adicionar ou remover uma classe */
+  if (tamanhoTela < 500) {
+    // Se for menor que o tamanho escolhido ele remove a classe que está com display:none (esconder)
+    footerResponsivo.classList.remove('toggleFooterResponsivo');
+    redesAntiga.classList.add('toggleFooterResponsivo');
+    
+  } else {
+    // Se não for menor que o tamanho escolhido ele adiciona a classe com display: none
+    footerResponsivo.classList.add('toggleFooterResponsivo');
+    redesAntiga.classList.add('toggleFooterResponsivo');
+  }
+  /* Ou seja, esse código diz que se o tamanho da tela for menor que X, ele irá mostrar o footer responsivo, e se for maior ele remove. */
+})
+
+gsap.from(".menu", {
+  opacity: 0,
+  y: 20,
+  duration: 1,
+  stagger: 0.1
+});
+
+gsap.from(".container", {
+  scrollTrigger: ".container",
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  delay:0.9
+});
+
+
+
+gsap.from(".texto", {
+  scrollTrigger: ".texto",
+  opacity: 0,
+  y: 20,
+  stagger: 0.2,
+  duration: 0.8,
+  delay:2
+});
+
+gsap.from(".container .container-images-banner3", {
+  duration: 0.9,
+  scale: 0.9,
+  y: 20,
+  opacity: 0,
+  stagger: 0.12,
+  ease: "back.out(1.2)",
+  delay:5
+});
+
+gsap.from(".container .agrupamento", {
+  scrollTrigger: ".banner_2",
+  duration: 0.9,
+  scale: 0.9,
+  y: 20,
+  opacity: 0,
+  stagger: 0.12,
+  ease: "back.out(1.2)",
+  delay:3
+});
+
+gsap.from(".banner_3", {
+  scrollTrigger: ".texto_3",
+  opacity: 0,
+  y: 20,
+  stagger: 0.2,
+  duration: 0.8,
+  delay:4
+});
+
+
+
+
+//menu hamburguer
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+  const menu = document.getElementById('menu-mobile');
+  menu.classList.toggle('ativo');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+window.onscroll = function() { updateProgressBar() };
+
+function updateProgressBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
